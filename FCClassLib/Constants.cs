3cb94e3ea4f01f9a4aa4997ca70f37853d2fe624
Frank Chu (Visual Studio) <frankchu@outlook.com>
2017-03-16 00:05:28 -0700

    US0000 - added constants and uriBuilder

---
+++ FCClassLib/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCClassLib
+{
+    public class Constants
+    {
+
+        public static class BasicHttp
+        {
+            public static string JsonContent = "application/json";
+
+            public static Dictionary<string, HttpMethod> MethodDict = new Dictionary<string, HttpMethod>()
+            {
+                 {"GET"     ,   HttpMethod.Get}
+                ,{"POST"    ,   HttpMethod.Post}
+                ,{"PUT"     ,   HttpMethod.Put}
+                ,{"DELETE"  ,   HttpMethod.Delete}
+                ,{"TRACE"   ,   HttpMethod.Trace}
+                ,{"HEAD"    ,   HttpMethod.Head}
+                ,{"OPTIONS" ,   HttpMethod.Options}
+            };
+        }
+
+        public static class GooglePlacesAPI
+        {
+
+        }
+    }
+}
---
+++ FCClassLib/FCUriHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace FCClassLib
+{
+    class FCUriHelper
+    {
+        private UriBuilder uriBuilder;
+        private NameValueCollection parameters;
+
+        public FCUriHelper()
+        {
+            uriBuilder = new UriBuilder();
+            parameters = HttpUtility.ParseQueryString(String.Empty);
+        }
+
+        public FCUriHelper(string uri)
+        {
+            uriBuilder = new UriBuilder(uri);
+            parameters = HttpUtility.ParseQueryString(String.Empty);
+        }
+
+        public FCUriHelper(string scheme, string host, int port, string pathValue)
+        {
+            uriBuilder = new UriBuilder(scheme, host, port, pathValue);
+            parameters = HttpUtility.ParseQueryString(String.Empty);
+        }
+
+        public void AddParameters(string key, string value)
+        {
+            parameters.Add(key, value);
+        }
+
+        public Uri GetUri()
+        {
+            if (parameters.Count > 0)
+            {
+                uriBuilder.Query = parameters.ToString();
+            }
+            return uriBuilder.Uri;
+        }
+    }
+}
